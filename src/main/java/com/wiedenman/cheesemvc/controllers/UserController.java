3004785ed4ebcc5909e5c4ddb0399a11929fd90e
landongw <landon@extantpublishing.com>
2017-11-28 22:55:26 -0600

    Stub out user classes and templates, add request mapping, user id, update footers in templates, user/add html form

--- src/main/java/com/wiedenman/cheesemvc/controllers/UserController.java
+++ src/main/java/com/wiedenman/cheesemvc/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.wiedenman.cheesemvc.controllers;
 
+import com.wiedenman.cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *  Landon Wiedenman
@@ -9,4 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)  // Displays form
+    public String displayAddUser(Model model) {
+        model.addAttribute("title", "Add User");
+        model.addAttribute(new User());
+
+        return "user/add";
+    }
 }
--- src/main/java/com/wiedenman/cheesemvc/models/User.java
+++ src/main/java/com/wiedenman/cheesemvc/models/User.java
@@ -1,14 +1,21 @@
 package com.wiedenman.cheesemvc.models;
 
 public class User {
+    private static int nextId = 1;
+    private final int id;
     private String username;
     private String email;
     private String password;
 
-    public User(String username, String email, String password) {
+    public User(int id, String username, String email, String password) {
+        this.id = nextId++;
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    public User() {
+        this.id = nextId++;
     }
 
     public String getUsername() {
