0713a57730520a0d2948c2deedab547479dcc932
landongw <landon@extantpublishing.com>
2017-11-28 22:17:00 -0600

    Start User Signup

---
+++ src/main/java/com/wiedenman/cheesemvc/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.wiedenman.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *  Landon Wiedenman
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+}
